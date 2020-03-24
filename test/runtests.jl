35e3b0b574bafb4789650efaba1465391e9b3b1a
korbi <korbinian90@gmail.com>
2020-03-24 16:00:48 +0100

    first running version

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
 using RomeoApp
 using Test
 
-@testset "RomeoApp.jl" begin
-    # Write your own tests here.
+@testset "ROMEO function tests" begin
+
+phasefile = joinpath("data", "small", "Phase.nii")
+magfile = joinpath("data", "small", "Mag.nii")
+
+function test_romeo(args)
+    file = tempname()
+    args = [args..., "-o", file]
+    unwrapping_main(args)
+    @test isfile(joinpath(file, "unwrapped.nii"))
+end
+
+args = [phasefile, "-B", "-t", "[2,4,6]"]
+test_romeo(args)
+
+args = [phasefile, "-m", magfile]
+test_romeo(args)
+
 end
